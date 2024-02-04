3f22d2116c871dd4eccb0058b80c83508f9d56e8
Amanda Garza <143903500+ajgarza44@users.noreply.github.com>
2024-02-03 21:26:38 -0600

    commit

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "30fbdbca01cff09f0ee72e9426e12c8c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "caf977f5272c4784bdccfef21786db6c"
